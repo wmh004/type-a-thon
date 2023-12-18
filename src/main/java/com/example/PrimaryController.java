d50225cecc63e468bce12af798690df65797760e
oatmeal <maxyjared@gmail.com>
2023-12-18 17:57:47 +0800

    Changed textbox to transparent and quit button works now

--- src/main/java/com/example/PrimaryController.java
+++ src/main/java/com/example/PrimaryController.java
@@ -1,12 +1,25 @@
 package com.example;
 
-import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 public class PrimaryController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private Button exit;
+    @FXML
+    private BorderPane mainPage;
+    @FXML
+    private Button refresh;
+
+    Stage stage;
+
+    public void quit(ActionEvent event) {
+        stage = (Stage) mainPage.getScene().getWindow();
+        stage.close();
     }
+
 }
