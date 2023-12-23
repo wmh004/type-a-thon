b18563c4d492f74ffcf171fad421d96f42f88a98
ndriannazriel04 <22002299@siswa.um.edu.my>
2023-12-23 22:44:53 +0800

    Some change from N

---
+++ src/main/java/com/example/GameController.java
@@ -0,0 +1,24 @@
+package com.example;
+
+public class GameController {
+    //need to allign the correct elements because to readjust
+    @FXML
+    public text seconds;
+
+    @FXML
+    public text wordsPerMin;
+
+    @FXML
+    public text accuracy;
+
+    @FXML
+    public text programWords;
+
+    @FXML
+    public text userWords;
+
+    @FXML
+    public Button playAgain;
+
+    
+}
