79ff0ae09a59e1498a05f283c707ce79fc2a1d5e
oatmeal <maxyjared@gmail.com>
2023-12-23 14:39:24 +0800

    Removed unused imports

--- src/main/java/com/example/MainPageController.java
+++ src/main/java/com/example/MainPageController.java
@@ -2,14 +2,8 @@ package com.example;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
