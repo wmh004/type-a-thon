f7cf12bed695352aff3b844ea00b49d9ed542c60
Wan <whisham04@gmail.com>
2023-12-25 16:08:28 +0800

    afterLoginPage fxml and controller is added

---
+++ src/main/java/com/example/afterLoginPageController.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class afterLoginPageController
+{
+    @FXML
+    private Button logOut;
+    @FXML
+    private Button back;
+
+    public void switchToMainPage(ActionEvent event) throws IOException {
+        App a = new App();
+        a.changeScene("mainpage.fxml");
+    }
+}
+
