99c52db5b3c7d97bec3f7b4a1e7e52e362832980
Wan <whisham04@gmail.com>
2024-01-01 22:13:15 +0800

    Adjusting player profile

--- src/main/java/com/example/playerProfileController.java
+++ src/main/java/com/example/playerProfileController.java
@@ -10,9 +10,16 @@ public class playerProfileController
 {
     @FXML
     private Button back;
+    @FXML
+    private Button next;
 
     public void switchtoLeaderboard(ActionEvent event) throws IOException {
         App a = new App();
         a.changeScene("leaderboard.fxml");
+    }
+
+    public void switchToNextPage(ActionEvent event) throws IOException {
+        App a = new App();
+        a.changeScene("playerProfile1.fxml");
     }
 }
--- src/main/java/com/example/playerProfileController1.java
+++ src/main/java/com/example/playerProfileController1.java
@@ -3,26 +3,15 @@ package com.example;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.MenuButton;
 
 import java.io.IOException;
 
-public class leaderboardController 
-{
-    @FXML
-    private Button playerHistory;
+public class playerProfileController1 {
     @FXML
     private Button back;
-    @FXML
-    private MenuButton gameMode;
 
     public void switchToPlayerProfile(ActionEvent event) throws IOException {
         App a = new App();
         a.changeScene("playerProfile.fxml");
-    }
-
-    public void switchToMainPage(ActionEvent event) throws IOException {
-        App a = new App();
-        a.changeScene("mainpage.fxml");
     }
 }
