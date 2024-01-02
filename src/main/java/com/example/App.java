b5e76be8d7deaa2419b7a4b396da0b714b29f434
Wan <whisham04@gmail.com>
2024-01-03 00:13:14 +0800

    Updates

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -33,7 +33,7 @@ public class App extends Application {
     }
 
     public void changeScene(String fxml) throws IOException {
-        Parent pane = FXMLLoader.load(getClass().getClassLoader().getResource(fxml));
+        Parent pane = FXMLLoader.load(getClass().getResource(fxml));
         stg.getScene().setRoot(pane);
     }
 
