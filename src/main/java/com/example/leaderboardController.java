43d9c554065f9cd9730f1cce328e072339bab4f9
Wan <whisham04@gmail.com>
2024-01-03 14:28:55 +0800

    Leaderboard

--- src/main/java/com/example/leaderboardController.java
+++ src/main/java/com/example/leaderboardController.java
@@ -3,23 +3,15 @@ package com.example;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.MenuButton;
 
+import java.io.*;
 import java.io.IOException;
 
 public class leaderboardController 
 {
     @FXML
-    private Button playerHistory;
-    @FXML
     private Button back;
-    @FXML
-    private MenuButton gameMode;
-
-    public void switchToPlayerProfile(ActionEvent event) throws IOException {
-        App a = new App();
-        a.changeScene("playerProfile.fxml");
-    }
+    
 
     public void switchToMainPage(ActionEvent event) throws IOException {
         App a = new App();
