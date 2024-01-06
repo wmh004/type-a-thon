35b78d8824dd4ae14f99e97c414dd332f5445142
pqistina <puteriqistina050204@gmail.com>
2024-01-06 20:49:39 +0800

    update

--- src/main/java/com/example/leaderboardController.java
+++ src/main/java/com/example/leaderboardController.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 public class leaderboardController {
 
-    private static final String DIRECTORY_PATH = "src\\main\\";
+    private static final String DIRECTORY_PATH = "src\\main\\java";
 
     @FXML
     private Button back;
@@ -125,112 +125,6 @@ public class leaderboardController {
                     username, avgWPM, avgACC, "%", bestWPM, bestACC, "%");
         }
     }
-        
-    //username from loginPageController, wpm and acc from ResultController
-    /*private String username;
-    private double wpm;
-    private double acc;
-
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setWPM(double wpm) {
-        this.wpm = wpm;
-    }
-
-    public void setACC(double acc) {
-        this.acc = acc;
-    }
-
-    //Results from ResultController
-    public void resultsToProfile(double wpm, double acc) {
-        setUsername(username);
-        setWPM(wpm);
-        setACC(acc);
-
-        File directory = new File(DIRECTORY_PATH);
-
-        for (File file : directory.listFiles()) {
-            if (file.isFile() && file.getName().startsWith(username) && file.getName().endsWith("Profile.txt")) {
-                String filePath = "src\\main\\java\\" + username + "Profile.txt";
-                List<String> lines;
-                try {
-                    lines = Files.readAllLines(Paths.get(filePath), StandardCharsets.UTF_8);
-                    
-                    for(int i = 0; i < lines.size(); i++) {
-                        String line = lines.get(i);
-                        lines.set(i, insertResults(line, username, wpm, acc));
-                        break;     
-                    }
-                    
-                    Files.write(Paths.get(filePath), lines, StandardCharsets.UTF_8);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
-    private static String insertResults(String line, String username, double wpm, double acc) {
-        double[] parts = convertToDoubleArray(line.split(","));
-        
-        //parts[0] username, parts[1] password, parts[2] avg wpm, parts[3] avg acc, parts[4] best wpm, parts[5] best acc
-        double avgWPM = parts[2]; double avgACC = parts[3];
-        double totalWPM = 0, totalACC = 0;
-
-        if(parts[24] != 0) { //If full, set the longest results to 0
-                parts[24] = 0;
-                parts[25] = 0;
-            }
-
-        for(int i = 24; i >= 8; i -= 2) { //Update 10 latest wpm and acc, odd is wpm, even is acc
-            parts[i] = parts[i - 2];
-            parts[i + 1] = parts[i - 1];
-        }
-
-        parts[6] = wpm; parts[7] = acc; //insert recent game wpm and acc
-
-        for(int i = 6; i < parts.length; i += 2) { //calculate average wpm and acc
-            totalWPM += parts[i];
-            totalACC += parts[i + 1];
-        }
-
-        avgWPM = totalWPM / 10.0; avgACC = totalACC / 10.0;
-        parts[2] = avgWPM; parts[3] = avgACC;
-
-        if(parts[2] > parts[4]) { //compare recent wpm and acc with best wpm and acc
-            parts[4] = parts[2];
-        }
-
-        if(parts[3] > parts[5]) {
-            parts[5] = parts[3];
-        }
-
-        return convertToString(parts);
-    }
-
-    private static double[] convertToDoubleArray(String[] credentials) {
-        double[] result = new double[credentials.length];
-        for(int i = 0; i < credentials.length; i++) {
-            result[i] = Double.parseDouble(credentials[i]);
-        }
-
-        return result;
-    }
-
-    private static String convertToString(double[] parts) {
-        StringBuilder builder = new StringBuilder();
-        for(int i = 0; i < parts.length; i++) {
-            builder.append(parts[i]);
-            if(i < parts.length - 1) {
-                builder.append(",");
-            }
-        }
-
-        return builder.toString();
-    }*/
 
     public void switchToMainPage(ActionEvent event) throws IOException {
         App a = new App();
